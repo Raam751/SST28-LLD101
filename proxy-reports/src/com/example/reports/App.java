4628be17ea59554fdff1aef109a3742fee0de41a
Raam <ydt751@gmail.com>
2026-03-29 18:00:51 +0530

    changed the readme

--- proxy-reports/src/com/example/reports/App.java
+++ proxy-reports/src/com/example/reports/App.java
@@ -3,7 +3,7 @@ package com.example.reports;
 public class App {
 
     public static void main(String[] args) {
-        User student = new User("Jasleen", "STUDENT");
+        User student = new User("Ram", "STUDENT");
         User faculty = new User("Prof. Noor", "FACULTY");
         User admin = new User("Kshitij", "ADMIN");
 
